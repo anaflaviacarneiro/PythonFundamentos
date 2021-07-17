b970d25629126a5a4c0ba91fc6256060df668d9f
Ana Flávia Carneiro <ana_lima04@hotmail.com>
2021-07-16 22:53:31 -0300

    Atualização CAP03

--- Cap03/Lab02/calculadora_v1.py
+++ Cap03/Lab02/calculadora_v1.py
@@ -6,4 +6,35 @@
 
 print("\n******************* Python Calculator *******************")
 
+print ("\nSelecione o número da operção desejada: \n")
 
+print("1 - Soma\n")
+print("2 - Subtração\n")
+print("3 - Multiplicação\n")
+print("4 - Divisão\n")
+
+opcao = input("Digite a sua opção (1/2/3/4): ")
+num1 = input("Digite o primeiro número: ")
+num2 = input("Digite o segundo número: ")
+numA = int(num1)
+numB = int(num2)
+
+    
+if opcao == '1':
+    resultado = numA + numB
+    print( numA, ' + ', numB, ' = ',resultado )
+    
+elif opcao =='2':
+    resultado = numA - numB
+    print( numA, ' - ', numB, ' = ',resultado )
+
+elif opcao =='3':
+    resultado = numA * numB
+    print( numA, ' * ', numB, ' = ',resultado )
+
+elif opcao =='4':
+    resultado = numA / numB
+    print( numA, ' / ', numB, ' = ',resultado )
+    
+else:
+    print("\n\n Opção inválida")
